66caac5d4cfd9871e2ac8b05ede598870716fd31
raghav-m007 <raghav.mittal@iitg.ac.in>
2022-07-17 15:11:11 +0530

    home page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import TopBar from "./topbar/TopBar";
+import Home from "./pages/home/Home";
+
+
+function App() {
+  return (
+    <>
+      <TopBar />
+      <Home />
+
+    </>
+  );
+}
+
+export default App;
