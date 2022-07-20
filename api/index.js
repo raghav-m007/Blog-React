6b2fba6e2251754d546de24a998d007a5c9db0c8
raghav-m007 <raghav.mittal@iitg.ac.in>
2022-07-20 21:18:18 +0530

    authorization

--- api/index.js
+++ api/index.js
@@ -9,10 +9,7 @@ app.use(express.json());
 
 mongoose
     .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: true
+
     }).then(console.log("Connected to MongoDB"))
     .catch((err) => console.log(err));
 
